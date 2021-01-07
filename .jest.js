add508048dd3b97a54f419eebdf3c18fc1e2bf01
cengcan <cengcan@fangdd.com>
2021-01-07 09:58:04 +0800

    delete .jest.js

--- .jest.js
+++
@@ -1,9 +0,0 @@
-// refer react-script test https://github.com/facebook/create-react-app/blob/282c03f952/packages/react-scripts/scripts/utils/createJestConfig.js
-module.exports = {
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['./setupTests.ts'],
-  "transform": {
-    "\\.[jt]sx?$": "babel-jest",
-    '^.+\\.(le)|css$': './jest/cssTransform.js',
-  },
-}

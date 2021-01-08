02dfaada8a2f7bddf718578a9a6caca4f612ca79
cengcan <cengcan@fangdd.com>
2021-01-08 17:46:28 +0800

    update

--- packages/can-ui/.jest.js
+++ packages/can-ui/.jest.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['./setupTests.ts'],
+  setupFilesAfterEnv: ['./setupTests.js'],
   "transform": {
     "\\.[jt]sx?$": "babel-jest",
     '^.+\\.(le)|css$': './jest/cssTransform.js',
